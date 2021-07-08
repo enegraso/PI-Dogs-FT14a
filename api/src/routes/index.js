7cd625680b3874d89e8609761002cfa6803f32ab
enegraso <sib2000@gmail.com>
2021-07-07 23:42:53 -0300

    me comi de guardar un archivo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -21,25 +21,23 @@ const router = Router();
 
 // ruta de temperamentos
 let temp = [];
-fetch(`https://api.thedogapi.com/v1/breeds/?api_key=${API_KEY}`)
+fetch(`https://api.thedogapi.com/v1/breeds/?api_key=${API_KEY}`) // Obtengo todos los temperamentos que se encuentran en las razas
     .then(response => response.json())
     .then(json => {
         json.map(tempe => {
-            if (tempe.temperament) {
-                let temps = tempe.temperament.split(', ');
-                temps.map(t => {
-                    if (!temp.find(tp => tp.name === t)) {
-                        temp.push({ name: t });
-
+            if (tempe.temperament) { // Solo si la raza tiene temperamentos
+                let temps = tempe.temperament.split(', '); // Como puede haber mas de un temperamento los separo por comma y espacio
+                temps.map(t => { // creo un nuevo array con los temperamentos
+                    if (!temp.find(tp => tp.name === t)) { // y si aún no está el temperamento en el array
+                        temp.push({ name: t }); // lo agrego 
                     }
                 });
             }
         })
     })
     .then(() => {
-        temp.map(t => {
-            console.log(t.name)
-            Temperaments.findOrCreate({
+        temp.map(t => { // los datos obtenidos en la promesa anterior
+            Temperaments.findOrCreate({ // Si ya está en la tabla queda o lo agrego si aun no está
                 where: {
                     name: t.name
                 }
