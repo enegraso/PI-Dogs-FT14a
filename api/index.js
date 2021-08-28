fffd8ee9259ca8efadf594b5008d762fa411cfee
Federico Ortiz <sib2000@gmail.com>
2021-08-28 17:57:36 -0300

    terminar

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-require('dotenv').config()
-//                   _oo0oo_
+//                       _oo0oo_
 //                      o8888888o
 //                      88" . "88
 //                      (| -_- |)
